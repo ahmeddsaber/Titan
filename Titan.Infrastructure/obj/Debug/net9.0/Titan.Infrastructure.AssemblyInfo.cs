940d54a1cbe54d3ff6b0931ef078e2e97d691147
Ahmed Saber <154698661+ahmeddsaber@users.noreply.github.com>
2026-04-18 20:55:04 +0200

    Add API Layer

--- Titan.Infrastructure/obj/Debug/net9.0/Titan.Infrastructure.AssemblyInfo.cs
+++ Titan.Infrastructure/obj/Debug/net9.0/Titan.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Titan.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f918bd35ef4ec9dba3f70e43c61ebd96dce755a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bdecbd406861594b0cdf3d37969ce48a6770934")]
 [assembly: System.Reflection.AssemblyProductAttribute("Titan.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Titan.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
