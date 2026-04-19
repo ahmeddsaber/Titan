a58e6dd22f43bfd1ec6fa9f3306619ec527c7a00
Ahmed Saber <154698661+ahmeddsaber@users.noreply.github.com>
2026-04-19 22:32:17 +0200

    AddRecommendationsController

---
+++ Titan.API/Controllers/RecommendationsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Titan.Application.Interfaces;
+
+namespace Titan.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class RecommendationsController : ControllerBase
+    {
+        private readonly IRecommendationService _recommendationService;
+        private Guid UserId => Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        public RecommendationsController(IRecommendationService recommendationService) { _recommendationService = recommendationService; }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] int count = 8) => Ok(await _recommendationService.GetRecommendationsAsync(UserId, count));
+    }
+
+}
--- Titan.API/obj/Debug/net9.0/Titan.API.AssemblyInfo.cs
+++ Titan.API/obj/Debug/net9.0/Titan.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Titan.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13af3a8188203bc678649812e41ed1abd37ed184")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecb95dcf3cdd9b151301061e53d7e0b4a7f625c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Titan.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Titan.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
