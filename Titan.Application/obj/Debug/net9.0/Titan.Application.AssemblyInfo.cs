586ec14ce3432c8847742549fd46d3eed1149d69
Ahmed Saber <154698661+ahmeddsaber@users.noreply.github.com>
2026-04-27 21:49:41 +0300

    Add For User Pages (Orders , Cart, Wishlist)

--- Titan.Application/obj/Debug/net9.0/Titan.Application.AssemblyInfo.cs
+++ Titan.Application/obj/Debug/net9.0/Titan.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Titan.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0499647b5c34c05d48278c16590e25f2d477ee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5719886b6af15184bbcbc9a72098b05c6cd9f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Titan.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Titan.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
